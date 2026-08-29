8e9b77eaef9814007d3ee4d7efb0b6bf7799040a
gawakawa <mashurakiryu@icloud.com>
2026-08-30 02:54:07 +0900

    :sparkles: Wire up purescript-react via react-basic-hooks
    Add react-basic / react-basic-dom / react-basic-hooks to nix/packages.nix
    using the with-react foreign-node_modules pattern from tic-tac-toe-purs,
    and mount a minimal <h1>todo</h1> component in Main.purs to prove it
    renders. Layout is unchanged; frontend/ consolidation follows separately.

--- nix/packages.nix
+++ nix/packages.nix
@@ -18,11 +18,48 @@
 
       purs-nix = inputs.purs-nix { inherit system; };
 
+      node_modules =
+        pkgs.importNpmLock.buildNodeModules {
+          npmRoot = ./..;
+          nodejs = pkgs.nodejs_24;
+        }
+        + /node_modules;
+
+      with-react =
+        package: modules:
+        pkgs.lib.recursiveUpdate package {
+          purs-nix-info.foreign = pkgs.lib.genAttrs modules (_: {
+            inherit node_modules;
+          });
+        };
+
       ps = purs-nix.purs {
         dependencies = [
           "ursi.debug"
           "effect"
           "prelude"
+
+          (with-react purs-nix.ps-pkgs.react-basic [
+            "React.Basic"
+            "React.Basic.StrictMode"
+          ])
+
+          (with-react purs-nix.ps-pkgs.react-basic-dom [
+            "React.Basic.DOM"
+            "React.Basic.DOM.Client"
+            "React.Basic.DOM.Components.GlobalEvents"
+            "React.Basic.DOM.Components.Ref"
+            "React.Basic.DOM.Events"
+            "React.Basic.DOM.Internal"
+            "React.Basic.DOM.Server"
+          ])
+
+          (with-react purs-nix.ps-pkgs.react-basic-hooks [
+            "React.Basic.Hooks"
+            "React.Basic.Hooks.Aff"
+            "React.Basic.Hooks.ErrorBoundary"
+            "React.Basic.Hooks.Suspense"
+          ])
         ];
 
         test-dependencies = [
--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,30 @@ module Main where
 
 import Prelude
 
-import Debug (todo)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Exception (throw)
+import React.Basic.DOM as R
+import React.Basic.DOM.Client (createRoot, renderRoot)
+import React.Basic.Hooks (Component, component)
+import React.Basic.Hooks as React
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
+
+mkApp :: Component Unit
+mkApp = component "App" \_ ->
+  React.do
+    pure $ R.h1_ [ R.text "todo" ]
 
 main :: Effect Unit
-main = todo
+main = do
+  app <- mkApp
+  doc <- document =<< window
+  root <- getElementById "root" $ toNonElementParentNode doc
+  case root of
+    Nothing -> throw "Could not find container element"
+    Just container -> do
+      reactRoot <- createRoot container
+      renderRoot reactRoot $ app unit
